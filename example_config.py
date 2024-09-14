d9cc97ed91166fe2341243d868adb494e1b72e9e
seansongss <seansongss@gmail.com>
2024-09-14 15:35:09 +0900

    example config

---
+++ example_config.py
@@ -0,0 +1,13 @@
+# Ticker symbols
+TICKER = 'AAPL'
+
+# Commission rate (e.g., $1 per trade)
+COMMISSION = 0.001
+
+# Strategy parameters
+SHORT_WINDOW = 20
+LONG_WINDOW = 50
+
+# Date range for backtesting
+START_DATE = '2022-01-01'
+END_DATE = '2023-09-01'
