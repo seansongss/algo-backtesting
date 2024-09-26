4929ead25af748fa90821c3c8479959631de2a35
Sean Song <seansongss@gmail.com>
2024-09-26 09:33:50 +0900

    add if it is airgapped network

--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -1,6 +1,13 @@
 import yfinance as yf
-from config import START_DATE, END_DATE, TICKER
+from config import START_DATE, END_DATE, TICKER, IS_AIRGAPPED
+import requests
 
 def fetch_data(ticker=TICKER, start_date=START_DATE, end_date=END_DATE):
-    data = yf.download(ticker, start=start_date, end=end_date)
+    session = None
+    # configure session if it airgapped network
+    if IS_AIRGAPPED:
+        session = requests.Session()
+        session.verify = False
+
+    data = yf.download(ticker, start=start_date, end=end_date, session=session)
     return data
