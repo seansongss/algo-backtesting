9f8e9560643b663abcf2c52f1d3ca32af58edc6c
Sean Song <seansongss@gmail.com>
2024-09-26 17:08:00 +0900

    disable warning in airgapped network

--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -1,5 +1,6 @@
 import requests
 import yfinance as yf
+import urllib3
 
 from config import START_DATE, END_DATE, TICKER, IS_AIRGAPPED
 
@@ -9,6 +10,9 @@ def fetch_data(ticker=TICKER, start_date=START_DATE, end_date=END_DATE):
     if IS_AIRGAPPED:
         session = requests.Session()
         session.verify = False
+
+        # Suppress only the specific InsecureRequestWarning
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     data = yf.download(ticker, start=start_date, end=end_date, session=session)
     return data
